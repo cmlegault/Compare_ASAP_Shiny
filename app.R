faf0e4b2c047e37e150b2c1636aa594b696f5057
cmlegault <chris.legault@noaa.gov>
2018-08-13 14:30:34 -0400

    add note that index sel block info not available

--- app.R
+++ app.R
@@ -171,6 +171,7 @@ server <- function(input, output) {
          if (iselb > 1) selblockres <- rbind(selblockres, meanselblock)
        }
        if (nselblocks > 1) selblockres <- apply(selblockres, 2, mean, na.rm=TRUE)
+       # note cannot include index selectivity block information because rdat file does not distinguish which selectivity option is used for each fleet
        
        # Phases
        phase.names <- c(names(asap$control.parms$phases), "Fleet.sel.blocks")
